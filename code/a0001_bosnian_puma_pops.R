3dfef2cd8fb53841eec9a0e95556512afb9a50c1
John Palmer <johnrbpalmer@gmail.com>
2018-02-26 00:45:57 +0100

    puma pops

--- code/a0001_bosnian_puma_pops.R
+++ code/a0001_bosnian_puma_pops.R
@@ -1,3 +1,5 @@
+rm(list=ls())
+
 library(data.table)
 library(tidyverse)
 library(Hmisc)
@@ -6,23 +8,25 @@ p2p = read_csv("~/research/acs/data/puma2puma.csv", skip=1)
 names(p2p) = names(read_csv("~/research/acs/data/puma2puma.csv"))
 p2p = p2p %>% mutate(stint = as.integer(state), puma12int = as.integer(puma12), puma2kint = as.integer(puma2k))
 
-p2co = read_csv("~/research/acs/data/puma2county.csv", skip=1) 
-names(p2co) = names(read_csv("~/research/acs/data/puma2county.csv"))
-p2co = p2co %>% mutate(stint = as.integer(state), puma2kint = as.integer(puma2k))
-
-p12_2_co = read_csv("~/research/acs/data/puma12_2_county.csv", skip=1) 
-names(p12_2_co) = names(read_csv("~/research/acs/data/puma12_2_county.csv"))
-p12_2_co = p12_2_co %>% mutate(stint = as.integer(state), puma12int = as.integer(puma12))
+D = as.tibble(readRDS(file="~/research/acs/data/ss09pus_POBP_bos.Rds")) %>% mutate(PINCP_r=replace(PINCP, PINCP == "bbbbbbb", NA), ADJINC_r=ADJINC*.000001, PINCP_ADJ_r = PINCP_r * ADJINC_r, ref_year=case_when(ADJINC==1119794~2005, ADJINC==1080505~2006, ADJINC==1051849~2007, ADJINC==1014521~2008, ADJINC==999480~ 2009), nat=CIT==4, LANX_r=factor(LANX), ENG_r=factor(ENG), FER_yes=FER==2, SEX_female=SEX==2, YOEP_r=replace(YOEP, YOEP=="bbbb", NA), yse=ref_year-YOEP_r, entry_period=case_when(YOEP<1992~"pre_war", YOEP %in% 1992:1995~"war", YOEP %in% 1996:1999~"immediate_post_war", YOEP>1999~"post_war"), working_age=AGEP%in%15:64, working_age_income=if_else(working_age, PINCP_ADJ_r, NULL)) 
 
 
-D = as.tibble(readRDS(file="~/research/acs/data/ss09pus_POBP_bos.Rds")) %>% mutate(PINCP_r=replace(PINCP, PINCP == "bbbbbbb", NA), ADJINC_r=ADJINC*.000001, PINCP_ADJ_r = PINCP_r * ADJINC_r, ref_year=case_when(ADJINC==1119794~2005, ADJINC==1080505~2006, ADJINC==1051849~2007, ADJINC==1014521~2008, ADJINC==999480~ 2009), nat=CIT==4, LANX_r=factor(LANX), ENG_r=factor(ENG), FER_yes=FER==2, SEX_female=SEX==2, YOEP_r=replace(YOEP, YOEP=="bbbb", NA), yse=ref_year-YOEP_r, entry_period=case_when(YOEP<1992~"pre_war", YOEP %in% 1992:1995~"war", YOEP %in% 1996:1999~"immediate_post_war", YOEP>1999~"post_war"), working_age=AGEP%in%15:64, working_age_income=if_else(working_age, PINCP_ADJ_r, NULL)) %>% left_join(p2co, by=c("ST"="stint", "PUMA"="puma2kint")) %>% mutate(county_r = if_else(county%in%c("29189", "29510"),"stlouis", county), cntyname_r=if_else(county%in%c("29189", "29510"),"St. Louis County & City, Missouri", cntyname))
+D16 = as.tibble(readRDS(file="~/research/acs/data/ss16pus_POBP_bos.Rds")) %>% mutate(PINCP_r=replace(PINCP, PINCP == "bbbbbbb", NA), ADJINC_r=ADJINC*.000001, PINCP_ADJ_r = PINCP_r * ADJINC_r, ref_year=case_when(ADJINC==1119794~2005, ADJINC==1080505~2006, ADJINC==1051849~2007, ADJINC==1014521~2008, ADJINC==999480~ 2009), nat=CIT==4, LANX_r=factor(LANX), ENG_r=factor(ENG), FER_yes=FER==2, SEX_female=SEX==2, YOEP_r=replace(YOEP, YOEP=="bbbb", NA), yse=ref_year-YOEP_r, entry_period=case_when(YOEP<1992~"pre_war", YOEP %in% 1992:1995~"war", YOEP %in% 1996:1999~"immediate_post_war", YOEP>1999~"post_war"), working_age=AGEP%in%15:64, working_age_income=if_else(working_age, PINCP_ADJ_r, NULL)) %>% left_join(p2p, by=c("ST"="stint", "PUMA"="puma12int")) 
 
-
-D16 = as.tibble(readRDS(file="~/research/acs/data/ss16pus_POBP_bos.Rds")) %>% mutate(PINCP_r=replace(PINCP, PINCP == "bbbbbbb", NA), ADJINC_r=ADJINC*.000001, PINCP_ADJ_r = PINCP_r * ADJINC_r, ref_year=case_when(ADJINC==1119794~2005, ADJINC==1080505~2006, ADJINC==1051849~2007, ADJINC==1014521~2008, ADJINC==999480~ 2009), nat=CIT==4, LANX_r=factor(LANX), ENG_r=factor(ENG), FER_yes=FER==2, SEX_female=SEX==2, YOEP_r=replace(YOEP, YOEP=="bbbb", NA), yse=ref_year-YOEP_r, entry_period=case_when(YOEP<1992~"pre_war", YOEP %in% 1992:1995~"war", YOEP %in% 1996:1999~"immediate_post_war", YOEP>1999~"post_war"), working_age=AGEP%in%15:64, working_age_income=if_else(working_age, PINCP_ADJ_r, NULL)) %>% left_join(p2p, by=c("ST"="stint", "PUMA"="puma12int")) %>% left_join(select(p12_2_co, stint, puma12int, county, cntyname), by=c("ST"="stint", "PUMA"="puma12int")) %>% mutate(county_r = if_else(county%in%c("29189", "29510"),"stlouis", county), cntyname_r=if_else(county%in%c("29189", "29510"),"St. Louis County & City, Missouri", cntyname))
+# 2009
 
 repcols = grep("PWGTP", names(D))
+
+main= D %>% select(g=PUMA, x=working_age_income, w=repcols[1]) %>% group_by(g) %>% summarise(mean_working_age_income=weighted.mean(x, w=w, na.rm=TRUE)) 
+
+test = bind_rows(lapply(2:length(repcols), function(i) D %>% select(PWGTP, ST=ST, PUMA=PUMA, x=working_age_income, w=repcols[i]) %>% group_by(ST, PUMA) %>% summarise(n = n(), mean_working_age_income_r=weighted.mean(x, w=w, na.rm=TRUE), mean_working_age_income=weighted.mean(x, w=PWGTP, na.rm=TRUE)))) %>% mutate(sq_diff= (mean_working_age_income_r-mean_working_age_income)^2, rep=i) %>% group_by(ST, PUMA,n, mean_working_age_income) %>% summarise(se=sqrt((4/80)*sum(sq_diff))) %>% filter(n>1)
+
+test
+
+
+
 i=1
-D %>% group_by(ST, county, cntyname) %>% select(repcols[i], working_age_income, yse, SEX_female, AGEP, nat, FER_yes) %>% summarise(N=sum(repcols[i]), mean_working_age_income=weighted.mean(working_age_income, weights=repcols[i], na.rm=TRUE), mean_yse=weighted.mean(yse, weights=repcols[i], na.rm=TRUE), pFemale=weighted.mean(SEX_female, weights=repcols[i], na.rm=TRUE), mean_age=weighted.mean(AGEP, weights=repcols[i], na.rm=TRUE), pnat=weighted.mean(nat, weights=repcols[i], na.rm=TRUE), pFER_yes=weighted.mean(FER_yes, weights=repcols[i], na.rm=TRUE)) %>% arrange(desc(N))
+D %>% group_by(ST, county, cntyname) %>% select(repcols[i], working_age_income, yse, SEX_female, AGEP, nat, FER_yes) %>% summarise(N=sum(repcols[i]), mean_working_age_income=weighted.mean(working_age_income, w=repcols[i], na.rm=TRUE), mean_yse=weighted.mean(yse, w=repcols[i], na.rm=TRUE), pFemale=weighted.mean(SEX_female, weights=repcols[i], na.rm=TRUE), mean_age=weighted.mean(AGEP, weights=repcols[i], na.rm=TRUE), pnat=weighted.mean(nat, weights=repcols[i], na.rm=TRUE), pFER_yes=weighted.mean(FER_yes, weights=repcols[i], na.rm=TRUE)) %>% arrange(desc(N))
 
 D %>% select(c("ST", "county_r", "cntyname_r"))
 
@@ -30,23 +34,6 @@ make_se = function(D, x, groups, rep_root="PWGTP"){
   D %>% group_by(groups) %>% select(grep(rep_root, names(.))) %>% summarise_all(sum)
 }
 
-
-cnty_ests = D %>% group_by(ST, county_r, cntyname_r) %>% select(PWGTP, working_age_income) %>% summarise(N=sum(PWGTP), med_working_age_income=median(working_age_income*PWGTP, na.rm=TRUE))
-cnty_reps = D %>% group_by(ST, county_r, cntyname_r) %>% select(grep("PWGTP", names(.))) %>% summarise_all(sum)
-ses = sqrt((4/80)*rowSums((cnty_ests %>% pull(N) - (cnty_reps %>% ungroup() %>% select(grep("PWGTP", names(.))) %>% select(2:81)))^2))
-cnty_ests$se = ses
-cnty_ests = cnty_ests %>% mutate(ci90ll = (N - 1.645*se), ci90ul = (N + 1.645*se), ci90ll = case_when(ci90ll<0~0, ci90ll>=0~ci90ll)) %>% arrange(desc(N))
-
-
-
-
-cnty_ests16 = D16 %>% group_by(ST, county_r, cntyname_r) %>% select(PWGTP) %>% summarise(N=sum(PWGTP))
-cnty_reps16 = D16 %>% group_by(ST, county_r, cntyname_r) %>% select(grep("PWGTP", names(.))) %>% summarise_all(sum)
-ses = sqrt((4/80)*rowSums((cnty_ests16 %>% pull(N) - (cnty_reps16 %>% ungroup() %>% select(grep("PWGTP", names(.))) %>% select(2:81)))^2))
-cnty_ests16$se = ses
-cnty_ests16 = cnty_ests16 %>% mutate(ci90ll = (N - 1.645*se), ci90ul = (N + 1.645*se), ci90ll = case_when(ci90ll<0~0, ci90ll>=0~ci90ll)) %>% arrange(desc(N))
-
-cnty_ests %>% full_join(cnty_ests16, by=c("ST", "county_r", "cntyname_r"), suffix=c("2009", "2016"))
 
 
 
